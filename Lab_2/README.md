0f166aec92fbe7f325c90778cf7feef8646af521
Kevin Hsiao <hsiaocombo@gmail.com>
2023-02-17 22:39:54 +0800

    Lab_2 complete code

---
+++ Lab_2/README.md
@@ -0,0 +1,5 @@
+Quick Start
+1. run `bash build.sh` to build server code under `/build`
+2. run `python3 demo.py` to run all testcases under `testcases`  
+
+see `spec` for more details
---
+++ Lab_2/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+python3 demo.py -t accept_invitation_sample
+python3 demo.py -t create_room_sample
+python3 demo.py -t exit_1_sample
+python3 demo.py -t exit_2_sample
+python3 demo.py -t exit_listinvitation
+python3 demo.py -t exit_listusers
+python3 demo.py -t exit_sample
+python3 demo.py -t gaming_multi
+python3 demo.py -t guess_sample
+python3 demo.py -t join_dismissed
+python3 demo.py -t join_room_sample
+python3 demo.py -t leave_room_sample
+python3 demo.py -t list_invitations_sample
+python3 demo.py -t list_rooms_and_users_sample
+python3 demo.py -t login_sample
+python3 demo.py -t logout_sample
+python3 demo.py -t managerexitgame
+python3 demo.py -t private_room_sample
+python3 demo.py -t register_sample
+python3 demo.py -t send_invitations_sample
+python3 demo.py -t start_game_sample
+python3 demo.py -t unsigned
